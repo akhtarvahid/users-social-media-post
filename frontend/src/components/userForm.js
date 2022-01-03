814cc8ad9e6cec59432ad434fda85299c0fede8e
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-01-03 22:34:00 +0530

    userForm component

--- frontend/src/components/userForm.js
+++ frontend/src/components/userForm.js
@@ -2,19 +2,30 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
 
-export default function UserForm({ user, handleInput, isFilled, handleSubmit }) {
-    return (
+function UserForm({ 
+  user, 
+  handleInput, 
+  isFilled, 
+  handleSubmit 
+}) {
+   const { firstName, lastName, workAt, designation } = user; 
+   return (
        <Box
          sx={{
           width: 500,
           maxWidth: '100%',
+          gap: 2,
          }}>
-         <TextField fullWidth label="First Name" id="firstName" name="firstName" onChange={handleInput}/>
-         <TextField fullWidth label="Last Name" id="lastName" name="lastName" onChange={handleInput}/>
-         <TextField fullWidth label="Work at" id="Work@" name="workAt" onChange={handleInput}/>
-         <TextField fullWidth label="Designation" id="designation" name="designation" onChange={handleInput}/>
-         <Button variant="contained" disabled={isFilled} onClick={handleSubmit}>Submit</Button>
+           <FormControl fullWidth>
+             <TextField margin='dense' label="First Name" name="firstName" onChange={handleInput} value={firstName} />
+             <TextField margin='dense' label="Last Name" name="lastName" onChange={handleInput} value={lastName} />
+             <TextField margin='dense' label="Work at" name="workAt" onChange={handleInput} value={workAt} />
+             <TextField margin='dense' label="Designation" name="designation" onChange={handleInput} value={designation} />
+             <Button style={{ marginTop: '10px' }} variant="contained" disabled={isFilled} onClick={handleSubmit}>Submit</Button>
+           </FormControl>
        </Box>
     )
 }
+export default UserForm;
