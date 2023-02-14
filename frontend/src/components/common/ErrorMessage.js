aacfa07a52cb306cfc01d5b13b01bae52db74ccc
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-02-14 08:58:22 +0530

    error component

--- frontend/src/components/common/ErrorMessage.js
+++ frontend/src/components/common/ErrorMessage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ErrorMessage() {
   return (
     <div>
-        <h1>Oops, Something went wrong!</h1>
+        Oops, Something went wrong!
     </div>
   )
 }
